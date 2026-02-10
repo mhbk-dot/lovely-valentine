6be5d985d5e7a32123fce41369173d381a26a7ff
mhbk-dot <mhbk@berkeley.edu>
2026-02-10 10:25:33 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Noi",
+    valentineName: "Noi ​🌟​",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? <3",
+    pageTitle: "To my lovely <3",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -50,8 +50,8 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Hi my love, thank you for being an amazing partner and I hope to spend many more cheesy Valentine's Days with you. From your love, Madi",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        message: "Hi my love, thank you for being an amazing human and I hope to spend many more cheesy Valentine's Days with you. From your love, Madi",
+        emojis: "❤️‍🔥​😏​😘​😽​😼​😻​🌟​💘"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "hthttps://res.cloudinary.com/dl7gknlx5/video/upload/v1770504559/keshi_-_Soft_Spot__Official_Music_Video__128k_zjjrlx.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
